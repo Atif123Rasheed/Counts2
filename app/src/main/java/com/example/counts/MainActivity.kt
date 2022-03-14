a93236de7a582526617a19051cf2feba09480f6d
Atif <arashid@leeddev.io>
2022-03-14 14:51:51 +0500

    First Commit

---
+++ app/src/main/java/com/example/counts/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.counts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val click_me = findViewById(R.id.push) as Button
+        val myTextView = findViewById(R.id.textView) as TextView
+        var timesClicked = 0
+        click_me.setOnClickListener{
+            timesClicked = timesClicked + 1
+            myTextView.text = timesClicked.toString()
+            Toast.makeText(this@MainActivity, "count clicked", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+}
