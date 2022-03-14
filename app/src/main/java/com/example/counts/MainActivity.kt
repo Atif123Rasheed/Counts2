0113833a8ca2a388181eef18c79909ff52fd4840
Bilal_Ranjha12 <mbilal@leeddev.io>
2022-03-14 16:21:15 +0500

    verify

--- app/src/main/java/com/example/counts/MainActivity.kt
+++ app/src/main/java/com/example/counts/MainActivity.kt
@@ -1,24 +1,29 @@
 package com.example.counts
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        setContentView(R.layout.activity_main)
+        var mediaPlayer: MediaPlayer? = null
         val click_me = findViewById(R.id.push) as Button
         val myTextView = findViewById(R.id.textView) as TextView
         var timesClicked = 0
+
         click_me.setOnClickListener{
+            mediaPlayer = MediaPlayer.create(this, R.raw.button)
+            mediaPlayer?.start()
             timesClicked = timesClicked + 1
             myTextView.text = timesClicked.toString()
             Toast.makeText(this@MainActivity, "count clicked", Toast.LENGTH_SHORT).show()
-
         }
     }
 }
